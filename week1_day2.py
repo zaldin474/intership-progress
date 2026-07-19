6e0472d2beb13bfa0b512045c9a2b931d88aa5e9
zaldin474 <zaldin474@gmail.com>
2026-07-19 10:44:55 +0300

    commit  jupy.ipynb commit week1_day2.py

---
+++ week1_day2.py
@@ -0,0 +1,20 @@
+
+def check_even_odd(num):
+    if num % 2 == 0:
+        return f"{num} is even"
+    else:
+        return f"{num} is odd"
+    
+def factorial(n):
+    if n ==0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def main():
+    for value in [1, 2, 3, 4]:
+        print(check_even_odd(value))
+    
+    print(factorial(5))
+    
+main()
